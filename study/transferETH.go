2c58132ade43857a8435b629c8a3f885c0df4504
yang.lin <ylstrive@163.com>
2025-11-12 16:37:07 +0800

    2025-11-12 ETH 转账

---
+++ study/transferETH.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"math/big"
+)
+
+func main() {
+	//获取到客户端
+	client, err := ethclient.Dial("https://mainnet.infura.io/v3/e7cbd13b843d4556bb9947cbf12cdbda")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//加载自己的私钥
+	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//根据私钥获取到公钥
+	publicKey := privateKey.Public()
+
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+	//根据公钥 获取到 帐户的公共地址
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	//获取到随机数
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//单位wei 0.01eth
+	value := big.NewInt(1000000000000000)
+	//ETH 转账的燃气应设上限为“21000”单位。
+	gasLimit := uint64(21000)
+
+	//燃气价格 用于根据'x'个先前块来获得平均燃气价格。
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	//转给谁 转入的 地址
+	toAddress := common.HexToAddress("0x4592d8f8d7b001e72cb26a73e4fa1806a51ac79d")
+	var data []byte
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//是使用发件人的私钥对事务进行签名
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//将已签名的事务广播到整个网络。
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", signedTx.Hash().Hex())
+}
